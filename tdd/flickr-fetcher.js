49add53b8aab617f195283fb339aa7ceab047779
Fiona <fiona@zentri.com>
2018-03-12 15:27:14 +1100

    Set up base tests

---
+++ tdd/flickr-fetcher.js
@@ -0,0 +1,7 @@
+var FlickrFetcher = {
+  photoObjToURL: function (photoObj) {
+    return 'https://farm' + photoObj.farm + '.staticflickr.com/' + photoObj.server + '/' + photoObj.id + '_' + photoObj.secret + '_b.jpg';
+  }
+};
+
+module.exports = FlickrFetcher;
